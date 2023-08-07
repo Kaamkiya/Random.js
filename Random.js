0193104f35a5828a53f38a5f694f56482c015ecb
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-08-07 07:50:06 -0400

    Update Random.js

--- Random.js
+++ Random.js
@@ -1,14 +1,21 @@
 const Random = {
-	integer: function(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.random() * (max - min + 1) + min; 
-	},
-	shuffle: function(array) {
-    		for (let i = array.length - 1; i > -1; i--) {
-      			const j = Random.integer(i + 1, 0);
-      			[array[i], array[j]] = [array[j], array[i]];
-    		}
-    		return array;
-	}
-}
+  int: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.random() * (max - min + 1) + min;
+  },
+  shuffle: function(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  },
+  choice: function(thing) {
+    if (typeof thing == 'string' || Array.isArray(thing)) {
+      return thing[Math.floor(Random.int(0, thing.length - 1))];
+    } else {
+      return `cannot pick from ${typeof thing}`;
+    }
+  },
+};
