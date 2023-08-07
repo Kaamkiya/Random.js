2a11231fb6835c83c0b31502586a4cbe4b343f09
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-08-07 07:09:07 -0400

    Update Random.js

--- Random.js
+++ Random.js
@@ -2,7 +2,7 @@ const Random = {
 	integer: function(min, max) {
 		min = Math.ceil(min);
 		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min + 1)) + min; 
+		return Math.random() * (max - min + 1) + min; 
 	},
 	shuffle: function(array) {
     		for (let i = array.length - 1; i > -1; i--) {
